bfa0a1d48e1a774f4b9c9285db7a3d9b8a57adfb
clovisjohn <77328569+clovisjohn@users.noreply.github.com>
2021-05-08 07:18:04 +0000

    Update V.01.py

--- releases/list matching/V.01.py
+++ releases/list matching/V.01.py
@@ -1,40 +1,10 @@
 import requests
 import re
 import json
-
 import spacy
 from bs4 import BeautifulSoup
-
-
-def coingecko_list():
-    '''
-return the latest coingecko tokens list
-    '''
-    f = requests.get("https://api.coingecko.com/api/v3/coins/list")
-    data = json.loads(f.text)
-    
-    return [i["symbol"].upper() for i in data] + [j["name"] for j in data]
-
-
-def msg_list(m):
-    '''
-take a string as input, the string is the chat history file path
-return list of messages
-    '''
-    with open(m, encoding="utf8") as fp:
-        soup = BeautifulSoup(fp, 'html.parser')
-        
-    
-    l=[]
-    
-    for i in soup.find_all('div', class_='text'):
-        temp=i.get_text()
-        temp=re.sub("\\n","",temp)
-        temp=temp.rstrip()
-        l.append(temp)
-        
-    return " ".join(l)
-
+from modules.functions import *
+from modules.tokenlists import *
 
 
 def token_list2(m,checklist=coingecko_list()):
