be26d78f3c8d340ea95c00662fc13b75cf0507ee
clovisjohn <77328569+clovisjohn@users.noreply.github.com>
2021-05-09 20:21:43 +0000

    Update launch.py

--- releases/list matching/launch.py
+++ releases/list matching/launch.py
@@ -1,5 +1,11 @@
 from main import *
-from .modules.uniswap_list import *
-from .modules.pancakeswap_list import *
+from modules.uniswap_list import *
+from modules.pancakeswap_list import *
+
+if uniswap_list()==None:
+    build_uniswap_list()
+if pancakeswap_list()==None:
+    build_pancakeswap_list()
+    
 
 token_list2("messages.html",full_list())
