217cf24c61efd99972ef63c9dd7233a0d1649cea
clovisjohn <77328569+clovisjohn@users.noreply.github.com>
2021-05-09 13:20:36 +0000

    Update uniswap_list.py

--- releases/list matching/modules/uniswap_list.py
+++ releases/list matching/modules/uniswap_list.py
@@ -23,22 +23,25 @@ def build_uniswap_list():
         
         
 def update_uniswap_list():
-    with open("uniswap tickers.txt", 'r') as w:
+
+    with open("uniswap tickers.txt", 'r') as w:  ##get old list
         old_list=w.read().splitlines() 
-    l=[] #dummy list to store token data
-    
-    k=["1",old_list[-1]] #list of ID , ID=token address
-    while k[-1]!=k[-2]:                          #till last id different from previous last Id
-        t_query=query_builder(k[-1])  #build a valid uniswap query to retrieve a token list from uniswap
-        temp=query_graph(t_query)     #use the previous query to make an api request
+        
+####### build new list starting from the last element of the old list
+    l=[]
+    k=["1",old_list[-1]]
+    while k[-1]!=k[-2]:                          
+        t_query=query_builder(k[-1])  
+        temp=query_graph(t_query)     
         l=l + temp["data"]["tokens"]
         try:
-            lastId=temp["data"]["tokens"][-1]["id"] #get Id of the last element of the generated token list
+            lastId=temp["data"]["tokens"][-1]["id"] 
         except IndexError:
             break
-        k.append(lastId)                     #add the last Id to the list of ID
+        k.append(lastId)                     
     
-    new_list=[i["symbol"].upper() for i in l] #extract tickers from the dummy list and add the Id of the last scraped token
-    f_list=old_list + new_list + [k[-1]]
+    new_list=[i["symbol"].upper() for i in l]
+
+    f_list=old_list[:-1] + new_list + [k[-1]]  ### merging the old and new lists
     with open("uniswap tickers.txt", 'w') as w:
-        w.writelines(s + "\n" for s in f_list)
+        w.writelines(s + "\n" for s in f_list) 
