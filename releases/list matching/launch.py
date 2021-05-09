313dbd5ab0a5615b6cb6195f8bca3a1c81c8ac90
clovisjohn <77328569+clovisjohn@users.noreply.github.com>
2021-05-09 20:45:36 +0000

    Update launch.py

--- releases/list matching/launch.py
+++ releases/list matching/launch.py
@@ -3,8 +3,10 @@ from modules.uniswap_list import *
 from modules.pancakeswap_list import *
 
 if uniswap_list()==None:
+    print("Building it...")
     build_uniswap_list()
 if pancakeswap_list()==None:
+    print("Building it...")
     build_pancakeswap_list()
     
 
