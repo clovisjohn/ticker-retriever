e7bb9b41b9df5a090df9105ad1ce93b8b6faf14d
clovisjohn <77328569+clovisjohn@users.noreply.github.com>
2021-05-09 15:05:04 +0000

    Update tokenlists.py

--- releases/list matching/modules/tokenlists.py
+++ releases/list matching/modules/tokenlists.py
@@ -5,11 +5,11 @@ def coingecko_list():
     
 def uniswap_list():
     try:
-        with open("unisnwap tickers.txt", 'r') as w:
+        with open("uniswap tickers.txt", 'r') as w:
             temp=w.read().splitlines() 
         return temp
     except FileNotFoundError:
-        print("unisnwap tickers.txt not found")
+        print("uniswap tickers.txt not found")
         
  
 def pancakeswap_list():
